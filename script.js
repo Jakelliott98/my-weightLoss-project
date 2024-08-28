fe9843285949569fe0ac63abba3616fcdcc7a93e
John Elliott <john98elliott@gmail.com>
2024-08-28 20:14:50 +0100

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const age = document.getElementById('age').value;
+const weight = document.getElementById('weight').value;
+const height = document.getElementById('height').value;
+const gender = document.getElementById('age').value; //
+
+
